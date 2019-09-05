fd809576f6c1bcb0006c80ceb725bfbfe7922011
Mariana Soares <mariana.dsantos@al.infnet.edu.br>
2019-09-05 09:34:58 -0300

    Exercício 7
    Escreva um programa em Python que realiza operações de inclusão e remoção em listas.
    Seu programa deve perguntar ao usuário qual operação deseja fazer: (código)
    Mostrar lista;
    Incluir elemento;
    Remover elemento;
    Apagar todos os elementos da lista.
    Se a opção for a alternativa (a), seu programa deve apenas mostrar o conteúdo da lista.
    Se a opção for a alternativa (b), seu programa deve pedir o valor do elemento a ser incluído.
    Se a opção for a alternativa (c), seu programa deve pedir o valor do elemento a ser removido.
    E se a opção for a alternativa (d), deve-se apenas exibir se a operação foi concluída.

---
+++ exercicio_7.py
@@ -0,0 +1,16 @@
+"""Escreva um programa em Python que realiza operações de inclusão e remoção em listas.
+Seu programa deve perguntar ao usuário qual operação deseja fazer: (código)
+Mostrar lista;
+Incluir elemento;
+Remover elemento;
+Apagar todos os elementos da lista.
+Se a opção for a alternativa (a), seu programa deve apenas mostrar o conteúdo da lista.
+Se a opção for a alternativa (b), seu programa deve pedir o valor do elemento a ser incluído.
+Se a opção for a alternativa (c), seu programa deve pedir o valor do elemento a ser removido.
+E se a opção for a alternativa (d), deve-se apenas exibir se a operação foi concluída."""
+
+print("Escolha entre as opções abaixo: ")
+print("(a) - Mostrar Lista")
+print("(b) - Incluir Elemento")
+print("(c) - Remover Elemento")
+print("(d) - Apagar todos o elementos da lista")
